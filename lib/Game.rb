1817170fc80307a3706bf93934a3d9b300309fda
fragius <189782341+Fragius@users.noreply.github.com>
2025-05-26 07:15:15 +0300

    Add round method

--- lib/Game.rb
+++ lib/Game.rb
@@ -5,8 +5,15 @@ require_relative 'Player'
 
 class Game
   def initialize
-    Board.new
-    Player.new(gets.chomp, 'X')
-    Player.new(gets.chomp, 'O')
+    @board = Board.new
+    @players = [Player.new(gets.chomp, 'X'), Player.new(gets.chomp, 'O')]
+    puts @board.load
+  end
+
+  def round
+    @players[0].play(@board)
+    puts @board.load
+    @players[1].play(@board)
+    puts @board.load
   end
 end
