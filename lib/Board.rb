b09b2cec05552ed24eb896f9473883a5192b3f04
fragius <189782341+Fragius@users.noreply.github.com>
2025-05-28 11:29:03 +0300

    Create check method

--- lib/Board.rb
+++ lib/Board.rb
@@ -2,6 +2,7 @@
 
 class Board
   def initialize
+    @check_guide = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [8, 4, 0]]
     @board = [['#', '#', '#'], ['#', '#', '#'], ['#', '#', '#']]
   end
 
@@ -16,5 +17,15 @@ class Board
     else
       false
     end
+  end
+
+  def check
+    @@check_guide.each do |test|
+      board = @board.flatten
+      if board[test[0]] == board[test[1]] && board[test[0]] == board[test[2]] && board[test[0]] != '#'
+        return board[test[0]]
+      end
+    end
+    nil
   end
 end
